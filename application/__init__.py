81683e1f0092d5ef807093a6f8162decf26560f7
Rogincel Demata <roginceldemata@gmail.com>
2024-11-09 14:17:47 +0800

    Update __init__.py

--- application/__init__.py
+++ application/__init__.py
@@ -4,12 +4,6 @@ from flask import Flask
 from application.views.page1 import page1
 from application.views.page2 import page2
 
-DB_SERVER   = "localhost"
-DB_PORT     = "3306"
-DB_USERNAME = "root"
-DB_PASSWORD = ""
-DB_NAME     = "dmathz_template"
-
 
 def create_app():
     app = Flask(__name__) 
