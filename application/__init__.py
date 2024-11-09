e26f737b6f97977f25030d7c488ac474f3de6b57
Rogincel Demata <roginceldemata@gmail.com>
2024-11-09 17:26:23 +0800

    Update __init__.py

--- application/__init__.py
+++ application/__init__.py
@@ -1,16 +1,12 @@
 import os
 from flask import Flask
-
 from application.views.page1 import page1
 from application.views.page2 import page2
 
-
 def create_app():
     app = Flask(__name__) 
-    
     app.register_blueprint(page1)
     app.register_blueprint(page2)
-
     return app
 
 app = create_app()
